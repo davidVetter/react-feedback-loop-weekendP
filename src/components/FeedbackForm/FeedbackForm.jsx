e601984dbbd82b87d078de9dcdf8fcfbbb7c7775
David Vetter <ddvetter23@gmail.com>
2022-10-01 15:17:16 -0500

    POST for feedback object to database appears to be working, state clears after POST

--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -1,19 +1,32 @@
 import {useState} from 'react';
+import axios from 'axios';
 import Comments from './FormPieces/Comments';
 import Feeling from './FormPieces/Feeling';
 import Review from './FormPieces/Review';
 import Supported from './FormPieces/Supported';
 import Understanding from './FormPieces/Understanding';
+import { useDispatch, useSelector } from 'react-redux';
 
 function FeedbackForm() {
-    const [feeling, setFeeling] = useState('');
-    const [understanding, setUnderstanding] = useState('');
-    const [support, setSupport] = useState('');
-    const [comments, setComments] = useState('');
+    const feedback = useSelector(store => store.feedback);
+    const dispatch = useDispatch();
+    // const [feeling, setFeeling] = useState('');
+    // const [understanding, setUnderstanding] = useState('');
+    // const [support, setSupport] = useState('');
+    // const [comments, setComments] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log('This worked');
+        axios.post('/feedback', feedback)
+             .then((response) => {
+                dispatch({
+                    type: 'CLEAR_FEEDBACK'
+                });
+             })
+             .catch((err) => {
+                alert('Error adding feedback');
+             });
     }
 
     return (
